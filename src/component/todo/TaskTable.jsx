36f84738903c769bf32cb035ef59722ff0c23389
Kalimi Sultan <developersultan100@gmail.com>
2024-12-19 20:49:48 +0600

    task table jsx

---
+++ src/component/todo/TaskTable.jsx
@@ -0,0 +1,79 @@
+import { FaCircleDot } from "react-icons/fa6";
+import useToggle from "../../hook/useToggle";
+import Status from "../taskEdit/Status";
+import TaskName from "../taskEdit/taskName";
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+
+const TaskTable = () => {
+  const {
+    toggle: isEdit,
+    hangleToggle: handleEdit,
+    toggleRef: editRef,
+  } = useToggle();
+  const {
+    toggle: isStatus,
+    hangleToggle: handleStatus,
+    toggleRef: statusRef,
+  } = useToggle();
+
+  return (
+    <>
+      <table className="table-fixed w-full text-left border bg-gray-100">
+        <thead>
+          <tr className="border-y">
+            <th className="py-2 px-4 w-full">Name</th>
+            <th className="py-2 px-4 w-3/12">Status</th>
+            <th className="py-2 px-4 w-2/12">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="py-2 px-4 relative">
+              <ul>
+                <li className="flex items-center gap-3">
+                  <button
+                    onClick={handleStatus}
+                    ref={statusRef}
+                    className="p-1 text-slate-600 hover:bg-slate-300 rounded-md"
+                  >
+                    <FaCircleDot />
+                  </button>
+                  MERN Stack
+                </li>
+              </ul>
+
+              {isStatus && <TaskName />}
+            </td>
+
+            <td className="py-2 px-4">
+              <button className="px-3 py-0.5 text-sm font-medium rounded-full border border-yellow-400 bg-yellow-500 hover:bg-yellow-600 text-white mr-3">
+                Panding
+              </button>
+              <button className="px-3 py-0.5 text-sm font-medium rounded-full border border-blue-400 bg-blue-500 hover:bg-blue-600 text-white">
+                Completed
+              </button>
+            </td>
+
+            <td className="py-2 px-4 relative">
+              <button
+                onClick={handleEdit}
+                ref={editRef}
+                className="p-1.5 text-lg border text-black bg-slate-200 hover:bg-slate-300 rounded-md mr-3"
+              >
+                <FaRegEdit />
+              </button>
+
+              {isEdit && <Status />}
+
+              <button className="p-1.5 text-lg border text-black bg-slate-200 hover:bg-slate-300 rounded-md mr-3">
+                <FaRegTrashAlt />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TaskTable;
--- src/component/todo/Todo.jsx
+++ src/component/todo/Todo.jsx
@@ -1,14 +1,8 @@
-import {
-  FaPlus,
-  FaRegEdit,
-  FaRegMoon,
-  FaRegSun,
-  FaRegTrashAlt,
-} from "react-icons/fa";
+import { FaPlus, FaRegMoon, FaRegSun } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
 import { LiaProjectDiagramSolid } from "react-icons/lia";
 import { GrTemplate } from "react-icons/gr";
-import { FaCircleDot, FaRegTrashCan } from "react-icons/fa6";
+import { FaRegTrashCan } from "react-icons/fa6";
 import { FiBook, FiHelpCircle } from "react-icons/fi";
 import { GoHome, GoTasklist } from "react-icons/go";
 import { IoSearch, IoSettingsOutline } from "react-icons/io5";
@@ -17,8 +11,7 @@ import TodoNav from "./todoNav";
 import Modal from "../modal/Modal";
 import useToggle from "../../hook/useToggle";
 import useDark from "../../hook/useDark";
-import Status from "../taskEdit/Status";
-import TaskName from "../taskEdit/taskName";
+import TaskTable from "./TaskTable";
 
 const Todo = () => {
   //todo menu items
@@ -79,16 +72,7 @@ const Todo = () => {
 
   //state
   const { toggle, hangleToggle } = useToggle();
-  const {
-    toggle: isEdit,
-    hangleToggle: handleEdit,
-    toggleRef: editRef,
-  } = useToggle();
-  const {
-    toggle: isStatus,
-    hangleToggle: handleStatus,
-    toggleRef: statusRef,
-  } = useToggle();
+
   const { isDark, handleDark } = useDark();
 
   return (
@@ -164,60 +148,7 @@ const Todo = () => {
                   <h2 className="text-2xl font-bold">Todo Task</h2>
                 </div>
                 <div className="task-items mt-5">
-                  <table className="table-fixed w-full text-left border bg-gray-100">
-                    <thead>
-                      <tr className="border-y">
-                        <th className="py-2 px-4 w-full">Name</th>
-                        <th className="py-2 px-4 w-3/12">Status</th>
-                        <th className="py-2 px-4 w-2/12">Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className="py-2 px-4 relative">
-                          <ul>
-                            <li className="flex items-center gap-3">
-                              <button
-                                onClick={handleStatus}
-                                ref={statusRef}
-                                className="p-1 text-slate-600 hover:bg-slate-300 rounded-md"
-                              >
-                                <FaCircleDot />
-                              </button>
-                              MERN Stack
-                            </li>
-                          </ul>
-
-                          {isStatus && <TaskName />}
-                        </td>
-
-                        <td className="py-2 px-4">
-                          <button className="px-3 py-0.5 text-sm font-medium rounded-full border border-yellow-400 bg-yellow-500 hover:bg-yellow-600 text-white mr-3">
-                            Panding
-                          </button>
-                          <button className="px-3 py-0.5 text-sm font-medium rounded-full border border-blue-400 bg-blue-500 hover:bg-blue-600 text-white">
-                            Completed
-                          </button>
-                        </td>
-
-                        <td className="py-2 px-4 relative">
-                          <button
-                            onClick={handleEdit}
-                            ref={editRef}
-                            className="p-1.5 text-lg border text-black bg-slate-200 hover:bg-slate-300 rounded-md mr-3"
-                          >
-                            <FaRegEdit />
-                          </button>
-
-                          {isEdit && <Status />}
-
-                          <button className="p-1.5 text-lg border text-black bg-slate-200 hover:bg-slate-300 rounded-md mr-3">
-                            <FaRegTrashAlt />
-                          </button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <TaskTable />
                 </div>
 
                 <div className="todo-add ">
